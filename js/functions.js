9e0e00869bf94f9ef8e9c3460a7076e47370321e
Shamrat <shomatemirbulatov@mail.ru>
2024-11-29 22:48:04 +0400

    строку в число

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,27 @@ const isPalindrome = (string = '') => {
     return string === reverseString;
   }
 };
+function extractsNumbers(string = '') {
+
+  let str = '';
+  let newStr = string.toString();
+
+  for (let i = 0; i <= newStr.length - 1; i++) {
+    if (parseInt(newStr[i], 10) || parseInt(newStr[i], 10) === 0) {
+      str += newStr[i];
+
+    }
+  }
+  if (str) {
+    let num = Number(str);
+    return Math.abs(num);
+  } else {
+    return NaN;
+  }
+}
+console.log(extractsNumbers('2023 год'));
+console.log(extractsNumbers('ECMAScript 2022'));
+console.log(extractsNumbers('1 кефир, 0.5 батона'));
+console.log(extractsNumbers('агент 007'));
+console.log(extractsNumbers('а я томат'));
+console.log(extractsNumbers(-1));
