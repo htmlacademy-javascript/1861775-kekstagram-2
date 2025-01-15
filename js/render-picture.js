ab41cb4fd5b3f26c9eaa6e51b6ecb1c8c905351d
Shamrat <shomatemirbulatov@mail.ru>
2025-01-15 14:02:35 +0400

    отрисуй меня полностью

---
+++ js/render-picture.js
@@ -0,0 +1,26 @@
+const pictureConteiner = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const fragment = document.createDocumentFragment();
+
+const renderPicture = function ({ id, url, description, likes, comment }) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  const image = pictureElement.querySelector('.picture__img');
+  const commentElement = pictureElement.querySelector('.picture__comments');
+  const likesElement = pictureElement.querySelector('.picture__likes');
+  image.dataset.photoId = id;
+  image.src = url;
+  image.alt = description;
+  commentElement.textContent = comment.length;
+  likesElement.textContent = likes;
+
+  return pictureElement;
+}
+
+const createRenderPictures = (arrayPhoto) => {
+  arrayPhoto.forEach((element) => {
+    fragment.appendChild(renderPicture(element));
+  });
+  pictureConteiner.appendChild(fragment);
+};
+
+export { createRenderPictures };
