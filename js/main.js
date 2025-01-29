2b1733062ea3e88bc51c4fdc9322d12d4845162d
Shamrat <shomatemirbulatov@mail.ru>
2025-01-29 15:32:46 +0400

    правда или действие

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 import { posts } from './data.js';
 import { createRenderPictures } from './render-picture.js';
 import { openModal } from './manipulation-modal.js';
+import { editPhotoScale } from "./photo-setting";
+import { closePhotoModal, editPhotoModal, postForm } from './edit-photo-modal.js';
 
 createRenderPictures(posts);
 openModal(posts);
+editPhotoModal();
+closePhotoModal();
+editPhotoScale();
+postForm();
