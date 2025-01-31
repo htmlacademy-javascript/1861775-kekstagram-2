0bb6de7c31b5433b0d87cfcbbb06ea7ee15541fb
Shamrat <shomatemirbulatov@mail.ru>
2025-01-31 23:42:06 +0400

    надо подкачаться

--- js/add-user-photo.js
+++ js/add-user-photo.js
@@ -18,7 +18,7 @@ const updateFilter = (filter) => {
 // Обновление текущего масштаба
 const updateScale = (scale) => {
   currentScale = scale;
-  imgUploadPrewiew.style.setProperty('transform', scale(${ currentScale / 100}));
+  imgUploadPrewiew.style.setProperty('transform', `scale(${currentScale / 100})`);
 };
 
 const loadUserPhoto = () => {
@@ -33,8 +33,8 @@ const loadUserPhoto = () => {
 
     const effectsPreview = formElement.querySelectorAll('.effects__preview');
     for (const effect of effectsPreview) {
-      effect.setAttribute('style', background - image: url("${imgUploadPrewiew.src}"););
-}
+      effect.setAttribute('style', `background - image: url("${imgUploadPrewiew.src}");`);
+    }
   });
 };
 
@@ -48,14 +48,14 @@ const addUserPhoto = () => {
 
     userPhotoElement.setAttribute('src', imgUploadPrewiew.src);
     userPhotoElement.style.setProperty('filter', currentFilter || ''); // Применение текущего фильтра
-    userPhotoElement.style.transform = scale(${ currentScale / 100}); // Применение масштаба
+    userPhotoElement.style.transform = `scale(${currentScale / 100})`; // Применение масштаба
 
-  userPhoto.querySelector('.picture__likes').textContent = 0;
-  userPhoto.querySelector('.picture__comments').textContent = 0;
+    userPhoto.querySelector('.picture__likes').textContent = 0;
+    userPhoto.querySelector('.picture__comments').textContent = 0;
 
-  thumbnail.appendChild(userPhoto);
-  container.appendChild(thumbnail);
-});
+    thumbnail.appendChild(userPhoto);
+    container.appendChild(thumbnail);
+  });
 };
 
 export { loadUserPhoto, addUserPhoto, updateFilter, updateScale };
