df5031b0082c91c2179c60499405ad248090a5d7
Shamrat <shomatemirbulatov@mail.ru>
2025-02-03 14:19:39 +0400

    Правки4

--- js/manipulation-modal.js
+++ js/manipulation-modal.js
@@ -5,24 +5,28 @@ const modalElement = document.querySelector('.big-picture');
 const closeElement = modalElement.querySelector('.big-picture__cancel');
 const thumbnailsContainerElement = document.querySelector('.pictures');
 
+// Обработчик события нажатия клавиши Escape
 function onEscapeDown(evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    closeModal();
+    onModalClose();
   }
 }
 
-function closeModal() {
+// Закрытие модального окна
+function onModalClose() {
   modalElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onEscapeDown);
   clearComment();
 }
 
+// Открытие модального окна с полноразмерным фото
 const openModal = (photos) => {
   thumbnailsContainerElement.addEventListener('click', (evt) => {
     if (evt.target.matches('.picture__img')) {
       const foundPhotoElement = photos.find((photo) => photo.id === Number(evt.target.dataset.photoId));
+
       evt.preventDefault();
       modalElement.classList.remove('hidden');
       document.body.classList.add('modal-open');
@@ -30,7 +34,7 @@ const openModal = (photos) => {
     }
 
     document.addEventListener('keydown', onEscapeDown);
-    closeElement.addEventListener('click', closeModal);
+    closeElement.addEventListener('click', onModalClose);
   });
 };
 
